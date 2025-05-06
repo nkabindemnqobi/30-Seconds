55e83c29e6c77efc800b30ed0da4df75f7c04e91
Magalaweng Clive Mmakola <162808288+CliveBBD@users.noreply.github.com>
2025-05-06 22:46:48 +0200

    Clive/feat/2 backend authentication with google (#25)
    * Set up google auth redirect uri
    
    - Set up google application in google cloud console
    - Set up a redirect endpoint
      => Where google will send the authcode to later exchange for an
      Id_token
      => Return an object with the code and sessionId:
    
    * Exchange auth code for Id_token
    
    * Cache tokens on api
    
    * Add remove console logs
    
    * Rename migration
    
    * Ignore package lock files
    
    * Update merge conflicts results
    
    * Update session generation func to a lambda

---
+++ api/shared/functions/google-auth-helpers.function.js
@@ -0,0 +1,13 @@
+const crypto = require("crypto");
+
+const generateSessionId = () => {
+    const bytes = new Uint8Array(32);
+    crypto.getRandomValues(bytes);
+
+    let base64 = Buffer.from(bytes).toString('base64');
+    return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
+module.exports = {
+    generateSessionId,
+}
