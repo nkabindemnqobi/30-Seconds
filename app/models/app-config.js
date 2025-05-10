a34f4057252de43841ef25949d25fafb6aadcf90
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2025-05-11 00:03:08 +0200

    Remove token from appconfig

--- app/models/app-config.js
+++ app/models/app-config.js
@@ -3,7 +3,6 @@
 */
 export const applicationConfiguration = {
     redirectUrl: "",
-    token: "",
     apiBaseUrl: "http://30-seconds.af-south-1.elasticbeanstalk.com",
     home: "http://my-30-seconds.s3-website.af-south-1.amazonaws.com/lobby"
 }
