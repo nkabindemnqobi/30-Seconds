c15c0a2e696cad50717a7f3da9428a7cdf6281c0
MnqobiNkabinde <mnqobi.nkabinde@bbd.co.za>
2025-05-12 22:42:16 +0200

    testFix

--- app/models/app-config.js
+++ app/models/app-config.js
@@ -1,10 +1,20 @@
 /*
     Change @apiBaseUrl to local instances when in development
 */
+// export class ApplicationConfiguration {
+//     static redirectUrl;
+//     static apiBaseUrl = "http://30-seconds.af-south-1.elasticbeanstalk.com";
+//     static loginUrl = "http://my-30-seconds.s3-website.af-south-1.amazonaws.com";
+
+//     static setRedirectUrl(redirectUri) {
+//         ApplicationConfiguration.redirectUrl = redirectUri;
+//     }
+// }
+
 export class ApplicationConfiguration {
     static redirectUrl;
-    static apiBaseUrl = "http://30-seconds.af-south-1.elasticbeanstalk.com";
-    static loginUrl = "http://my-30-seconds.s3-website.af-south-1.amazonaws.com";
+    static apiBaseUrl = "http://localhost:8000";
+    static loginUrl = "http://localhost:3000";
 
     static setRedirectUrl(redirectUri) {
         ApplicationConfiguration.redirectUrl = redirectUri;
